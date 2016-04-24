c8d0f57fc6ddd8201ed94e0278c893842dadc513
sacreman <sacreman@gmail.com>
2016-04-24 15:13:50 +0100

    remove problematic long description

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
     author='Steven Acreman',
     author_email='steven.acreman@dataloop.io',
     description='A Python Slack Bot with hot patch!',
-    long_description=fread('README.md'),
     keywords="slack bot",
     packages=find_packages(exclude=['tests']),
     exclude_package_data={'': ['config.yaml']},
